119173761c2a0e0cad4057d82cca3954781230cd
shkundapn <maksmagur@gmail.com>
2025-12-26 23:27:07 +0200

    Update maksym.py

--- day3_sliding_window_linked_list/maksym.py
+++ day3_sliding_window_linked_list/maksym.py
@@ -34,7 +34,29 @@ def totalFruit(fruits: list[int]) -> int:
 
     return max_outcome
 
+def countGoodSubstrings(s: str) -> int:
+    """
+    Returns the number of good substrings of length three in s.
+    A substring is good if there are no repeated characters.
+
+    >>> countGoodSubstrings("xyzzaz")
+    1
+    >>> countGoodSubstrings("aababcabc")
+    4
+    >>> countGoodSubstrings("abc")
+    1
+    >>> countGoodSubstrings("aba")
+    0
+    >>> countGoodSubstrings("aa")
+    0
+    """
+    count = 0
+    for i in range(len(s) - 2):
+        if len(set(s[i:i+3])) == 3:
+            count += 1
+    return count
+
 
 if __name__ == "__main__" :
-    import doctestф
+    import doctest
     doctest.testmod()
