85aa389173998060a2f6b6d9ffb485eeb36b66bd
IhorKastranetsUCU <kastranets.pn@ucu.edu.ua>
2025-12-26 23:32:06 +0200

    Day 3: Sliding Window

--- day03_sliding_window/sliding_window.py
+++ day03_sliding_window/sliding_window.py
@@ -3,7 +3,7 @@ def countGoodSubstrings(s: str) -> int:
     The idea of static sliding window is simple. We take few items in the row
     and move the whole block at the time by subtracting the first value and adding the newest one.
 
-    This function solves the problem where tou need to find the count of sequences
+    This function solves the problem where you need to find the count of sequences
     with the total length of 3 and unique characters only
 
     >>> countGoodSubstrings("abscdsd")
